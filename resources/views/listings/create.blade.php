96c21ba54a922e23263d7eaf1daf337ac329c19a
Sourav Malo <sourav.cse5.bu@gmail.com>
2023-02-20 17:42:37 +0600

    old values kept into the form

--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -17,6 +17,7 @@
             type="text"
             class="border border-gray-200 rounded p-2 w-full"
             name="company"
+            value="{{ old('company') }}"
           />
           @error('company')
             <p class="text-xs text-red-500 mt-1">{{ $message }}</p>
@@ -30,6 +31,7 @@
             class="border border-gray-200 rounded p-2 w-full"
             name="title"
             placeholder="Example: Senior Laravel Developer"
+            value="{{ old('title') }}"
           />
           @error('title')
             <p class="text-xs text-red-500 mt-1">{{ $message }}</p>
@@ -45,6 +47,7 @@
             class="border border-gray-200 rounded p-2 w-full"
             name="location"
             placeholder="Example: Remote, Boston MA, etc"
+            value="{{ old('location') }}"
           />
           @error('location')
             <p class="text-xs text-red-500 mt-1">{{ $message }}</p>
@@ -59,6 +62,7 @@
             type="text"
             class="border border-gray-200 rounded p-2 w-full"
             name="email"
+            value="{{ old('email') }}"
           />
           @error('email')
             <p class="text-xs text-red-500 mt-1">{{ $message }}</p>
@@ -73,6 +77,7 @@
             type="text"
             class="border border-gray-200 rounded p-2 w-full"
             name="website"
+            value="{{ old('website') }}"
           />
           @error('website')
             <p class="text-xs text-red-500 mt-1">{{ $message }}</p>
@@ -88,6 +93,7 @@
             class="border border-gray-200 rounded p-2 w-full"
             name="tags"
             placeholder="Example: Laravel, Backend, Postgres, etc"
+            value="{{ old('tags') }}"
           />
           @error('tags')
             <p class="text-xs text-red-500 mt-1">{{ $message }}</p>
@@ -114,7 +120,7 @@
             name="description"
             rows="10"
             placeholder="Include tasks, requirements, salary, etc"
-          ></textarea>
+          >{{ old('description') }}</textarea>
           @error('description')
             <p class="text-xs text-red-500 mt-1">{{ $message }}</p>
           @enderror
