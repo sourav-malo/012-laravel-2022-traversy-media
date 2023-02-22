944f127d67fbdd7879acf06eff6f1786eb775a81
Sourav Malo <sourav.cse5.bu@gmail.com>
2023-02-22 19:47:03 +0600

    auth and guest middlewares introduced

--- routes/web.php
+++ routes/web.php
@@ -16,15 +16,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [ListingController::class, 'index']);
-Route::get('/listings/create', [ListingController::class, 'create']);
-Route::post('/listings', [ListingController::class, 'store']);
-Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
-Route::put('/listings/{listing}', [ListingController::class, 'update']);
-Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
+Route::get('/listings/create', [ListingController::class, 'create'])->middleware('auth');
+Route::post('/listings', [ListingController::class, 'store'])->middleware('auth');
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit'])->middleware('auth');
+Route::put('/listings/{listing}', [ListingController::class, 'update'])->middleware('auth');
+Route::delete('/listings/{listing}', [ListingController::class, 'destroy'])->middleware('auth');
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
 
-Route::get('/register', [UserController::class, 'create']);
-Route::post('/users', [UserController::class, 'store']);
-Route::get('/login', [UserController::class, 'login']);
-Route::post('/users/authenticate', [UserController::class, 'authenticate']);
-Route::delete('/logout', [UserController::class, 'destroy']);
+Route::get('/register', [UserController::class, 'create'])->middleware('guest');
+Route::post('/users', [UserController::class, 'store'])->middleware('guest');
+Route::get('/login', [UserController::class, 'login'])->middleware('guest')->name('login');
+Route::post('/users/authenticate', [UserController::class, 'authenticate'])->middleware('guest');
+Route::delete('/logout', [UserController::class, 'destroy'])->middleware('auth');
